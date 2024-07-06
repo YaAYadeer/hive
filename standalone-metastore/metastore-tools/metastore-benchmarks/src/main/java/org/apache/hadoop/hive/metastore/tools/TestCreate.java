18e62aa4cc5a69fbc260a162f688e11657f62660
YKUN <zhangyukun@kingsoft.com>
2024-07-06 17:23:57 +0800

    000

--- standalone-metastore/metastore-tools/metastore-benchmarks/src/main/java/org/apache/hadoop/hive/metastore/tools/TestCreate.java
+++ standalone-metastore/metastore-tools/metastore-benchmarks/src/main/java/org/apache/hadoop/hive/metastore/tools/TestCreate.java
@@ -1,8 +1,14 @@
 package org.apache.hadoop.hive.metastore.tools;
 
-import java.net.URISyntaxException;
+import org.apache.hadoop.hive.metastore.TableType;
+import org.apache.thrift.TException;
 
-import static org.apache.hadoop.hive.metastore.tools.Util.getServerUri;
+import java.net.URISyntaxException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.apache.hadoop.hive.metastore.tools.Util.*;
 
 /**
  * @Author: YKUN
@@ -11,13 +17,47 @@ import static org.apache.hadoop.hive.metastore.tools.Util.getServerUri;
  */
 public class TestCreate {
     public static void main(String[] args) throws Exception {
-        try (HMSClient client = new HMSClient(getServerUri(args[0],"9083"), "")) {
-            System.out.println("Start to make sure dbExists");
-            if (!client.dbExists(args[1])) {
-                System.out.println("=start  to createDatabase");
-                client.createDatabase(args[1]);
-                System.out.println("end to createDatabase");
-            }
+        Map<String, String> parameters = new HashMap<>(40);
+        for (int i = 0; i < 2; i++) {
+            parameters.put("key" + i, "value" + i);
+        }
+        for (int i = 1; i <= Integer.parseInt(args[2]); i++) {
+            new Thread(() -> {
+                System.out.println("Thread" + i);
+                System.out.println("Start to make sure dbExists");
+                try (HMSClient client = new HMSClient(getServerUri(args[0], "9083"), "")) {
+                    if (!client.dbExists(args[1])) {
+                        String dbName = args[1] + i;
+                        System.out.println("=start  to createDatabase " + dbName);
+                        client.createDatabase(dbName);
+                        System.out.println("end to createDatabase " + dbName);
+                        for (int j = 0; j < 1000; j++) {
+                            String tableName = "tname" + j;
+                            System.out.println("createTable " + tableName);
+                            client.createTable(
+                                    new TableBuilder(dbName, tableName)
+                                            .withType(TableType.MANAGED_TABLE)
+                                            .withColumns(createSchema(Collections.singletonList("name:string")))
+                                            .withPartitionKeys(createSchema(Collections.singletonList("date")))
+                                            .withParameter("transactional", "true")
+                                            .withInputFormat("org.apache.hadoop.hive.ql.io.orc.OrcInputFormat")
+                                            .withOutputFormat("org.apache.hadoop.hive.ql.io.orc.OrcOutputFormat")
+                                            .withSerde("org.apache.hadoop.hive.ql.io.orc.OrcSerde")
+                                            .build());
+                            System.out.println("addpart");
+                            addManyPartitions(client, dbName, tableName, parameters, Collections.singletonList("id"), 40);
+                            System.out.println("getTable " + tableName);
+                            client.getTable(dbName, tableName);
+                            System.out.println("dropDatabase " + dbName);
+                            client.dropDatabase(dbName);
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }).start();
+        }
     }
 }
-}
+
